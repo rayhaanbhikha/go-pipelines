2be146e2538a8c597ce22b89386f06480533a9c1
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-03-28 22:22:42 +0000

    read method added.

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
+
 	fmt.Println("hello world")
+	readData("./data-set.csv")
+}
+
+type user struct {
+	firstName, lastName, email string
+}
+
+func NewUser(data []string) *user {
+	return &user{firstName: data[0], lastName: data[1], email: data[2]}
+}
+
+func readData(filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	csvReader := csv.NewReader(file)
+	for {
+		data, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(NewUser(data))
+	}
 }
